b3160f3651f1fa0736cda4d2c28cce8c78b5c39f
wadalz1998 <thanhlocnguyen1402@gmail.com>
2023-11-06 22:09:22 +0700

    finished

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.getElementById("switchButton").onclick = function () {
+  //khi button được click thì tìm đến thẻ body có id là myBody
+  // dùng classlist để gọi thuộc tính class của thẻ body
+  //Sử dụng toggle() để thêm hoặc xóa 1 class của thẻ html. Nếu thẻ body không có class dark thì toggle sẽ thêm class dark vào body. Ngược lại, nếu thẻ body đang có class dark thì toogle sẽ xóa class dark đi.
+  document.getElementById("myBody").classList.toggle("dark");
+};
+
